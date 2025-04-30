53498dda1badd083019473496fe3d3e2f4a423f7
tuoiy3936 <tuoiy3936@naver.com>
2025-04-30 19:23:08 +0900

    first commit

--- 최은결/카피/SHINSEGAE FOOD/js/main.js
+++ 최은결/카피/SHINSEGAE FOOD/js/main.js
@@ -25,3 +25,30 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
